0677d46edc53e3b201c4f11e75792a365da7b401
mjmf1 <marcomora61@gmail.com>
2023-10-25 01:17:02 -0500

    aplicacion completada

---
+++ productos.sql
@@ -0,0 +1,60 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: localhost:3306
+-- Tiempo de generación: 25-10-2023 a las 05:14:18
+-- Versión del servidor: 8.0.23
+-- Versión de PHP: 8.0.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `datos`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `productos`
+--
+
+CREATE TABLE `productos` (
+  `id` int NOT NULL,
+  `nombre` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `precio` decimal(10,2) DEFAULT NULL,
+  `codigo` varchar(20) COLLATE utf8mb4_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `productos`
+--
+ALTER TABLE `productos`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `productos`
+--
+ALTER TABLE `productos`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- src/main/java/WebScraper.java
+++ src/main/java/WebScraper.java
@@ -4,18 +4,21 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class WebScraper {
     public static void main(String[] args) {
-        System.setProperty("webdriver.gecko.driver", "C:\\Users\\Mjmf\\Downloads\\scraper_java-master\\geckodriver.exe");
+        System.setProperty("webdriver.gecko.driver", "geckodriver.exe");
         WebDriver driver = new FirefoxDriver();
 
         try {
-            String url = "https://www.ktronix.com/computadores-tablet/computadores-portatiles/c/BI_104_KTRON"; // Reemplaza con la URL real
+            String url = "https://www.ktronix.com/computadores-tablet/computadores-portatiles/c/BI_104_KTRON";
             driver.get(url);
 
             WebDriverWait wait = new WebDriverWait(driver, 10);
@@ -23,21 +26,44 @@ public class WebScraper {
 
             Document doc = Jsoup.parse(driver.getPageSource());
 
+            // Establecer la conexión a la base de datos MySQL
+            String dbUrl = "jdbc:mysql://localhost:3306/datos";
+            String username = "root";
+            String password = "123456";
+            Connection connection = DriverManager.getConnection(dbUrl, username, password);
+            System.out.println("Conexión a la base de datos MySQL establecida.");
+
             Elements productItems = doc.select(".ais-InfiniteHits-item");
 
-            for (Element productItem : productItems) {
-                Element productTitleElement = productItem.select(".product_itemtop_title a").first();
-                String productTitle = productTitleElement.text();
+            // Limitar el bucle para procesar solo 5 productos
+            int productsToScrape = 5;
+            for (int i = 0; i < Math.min(productsToScrape, productItems.size()); i++) {
+                Element productItem = productItems.get(i);
+
+                Element productTitleElement = productItem.select(".product__item__top__title a").first();
+                String productName = productTitleElement.text();
                 String productCode = productTitleElement.attr("data-id");
 
-                Element productPriceElement = productItem.select(".product_price--discounts_price span.price").first();
-                String productPrice = productPriceElement.text().replace("$", "").replace(".", "");
+                Element productPriceElement = productItem.select(".product__price--discounts__price span.price").first();
+                String productPrice = productPriceElement.text().replace("$", "").replace(".", "").replace(",", "");
 
-                System.out.println("Nombre: " + productTitle);
+                // Insertar los datos en la base de datos
+                String insertQuery = "INSERT INTO productos (nombre, precio, codigo) VALUES (?, ?, ?)";
+                PreparedStatement preparedStatement = connection.prepareStatement(insertQuery);
+                preparedStatement.setString(1, productName);
+                preparedStatement.setDouble(2, Double.parseDouble(productPrice));
+                preparedStatement.setString(3, productCode);
+                preparedStatement.executeUpdate();
+
+                System.out.println("Nombre: " + productName);
                 System.out.println("Precio: $" + productPrice);
                 System.out.println("Código: " + productCode);
                 System.out.println("--------------");
             }
+            // Cerrar la conexión a la base de datos
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
         } finally {
             driver.quit();
         }
